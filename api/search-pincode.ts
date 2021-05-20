15c68a2b9238f32a3f535837ac06812891b3781a
Javed Dosani <javeddosani2011@gmail.com>
2021-05-20 21:57:18 +0530

    use pincodes

--- api/search-pincode.ts
+++ api/search-pincode.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import {pincodes} from './pincodes';
+import { pincodes } from './pincodes';
 export default async (req: VercelRequest, res: VercelResponse) => {
     res.json({ message: pincodes[0] });
 };
